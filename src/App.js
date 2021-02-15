db10cab822455051cd41bfdd474e98b1f47f7678
MeikaFreckelton <meikafreckelton@gmail.com>
2021-02-15 13:05:14 +1100

    responsive on all

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import Home from './components/Home.js'
 import "./assets/scss/style.css"
-
+import "./assets/scss/responsive.css"
 
 const App = () => {
 	
